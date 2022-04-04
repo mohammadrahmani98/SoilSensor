1f2b4ea1184d74dcc7698ea5eb3ed2f5424b4fce
Checco1 <s296579@studenti.polito.it>
2022-04-04 15:52:39 +0200

    Send/Reciver Program

--- SigFox_Test/Sender/main.py
+++ SigFox_Test/Sender/main.py
@@ -1,13 +1,14 @@
 from network import Sigfox
 import socket
+import ubinascii
 
 # RCZ1 = EUROPE RCZ2 = USA RCZ3 = Japan RCZ4 = Australia
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
-
+print(ubinascii.hexlify(sigfox.mac()))
 s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
 
 s.setblocking(True)
 
-s.setsockopt(socket.SOL_SIGFOX, socket.S0_RX, False)
+s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
 s.send('Hello !')
